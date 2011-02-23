49f3623a18e33f2ef8dc7dc53d90feecf73e175e
Dan Lash <danlash@gmail.com>
2011-02-22 21:05:23 -0500

    base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beerocrat::Application.config.secret_token = 'd083c2bec5f3bff53b0750c9da8df259c77ab9cc9ad49f34318a336c8d57b683e5f52a59404b312ce6f64c6b33e14ebd9117129cbe00240a38b6443fc5d75264'
