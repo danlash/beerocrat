e2cd9451a54ff34e2dc684275bc116c9e45b2118
Dan Lash <danlash@gmail.com>
2011-02-22 22:52:02 -0500

    working home page for current beer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
+  	b = Beer.arel_table
+	@current_beer = Beer.where(b[:date_emptied].eq(nil)).first
+
+	respond_to do |format|
+      format.html
+    end
   end
 
 end
