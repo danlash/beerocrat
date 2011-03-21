70b761eebd823ad8b4c9cf1c596c49f083e68f87
Dan Lash <danlash@gmail.com>
2011-03-21 09:20:14 -0400

    not sure

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   	b = Beer.arel_table
 	@current_beer = Beer.where(b[:date_emptied].eq(nil)).first
-
+	@past_beers = Beer.where(b[:date_emptied].gt(Date.new)).limit(5)
 	respond_to do |format|
       format.html
     end
